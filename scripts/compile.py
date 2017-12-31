ad6c954b485367c781a11180d6c201c5ac1c23f9
Abhi Agarwal <goldenercobra@gmail.com>
2017-12-31 13:51:14 -0500

    Update compile script
    Output to file

--- scripts/compile.py
+++ scripts/compile.py
@@ -4,12 +4,21 @@ import sass
 
 from add_images import custom_flair_images
 
-def main() -> str:
+def compile_sass() -> str:
+    """compiles sass index file"""
     index_file: Path = (Path.cwd().parent / "index.scss")
     return sass.compile(
         filename=str(index_file),
         custom_functions={custom_flair_images}
     )
 
+def main() -> None:
+    """outputs generated css to file"""
+    output: Path = (Path.cwd().parent / "output.css")
+    output.touch()
+
+    with output.open("w") as file:
+        file.write(compile_sass())
+
 if __name__ == "__main__":
-    print(main())
+    main()
