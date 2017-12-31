686fd2c49d52affb0b70e44e89142ada0c82b038
Abhi Agarwal <goldenercobra@gmail.com>
2017-12-31 15:55:33 -0500

    Read sidebar image name and dimensions automatically
    Avoids defining variables and deals directly

--- scripts/compile.py
+++ scripts/compile.py
@@ -4,13 +4,23 @@ from pathlib import Path
 import sass
 
 from add_images import custom_flair_images
+from find_sidebar import (
+    custom_sidebar_dimensions, 
+    custom_sidebar_name, 
+    custom_sidebar_exists
+)
 
 def compile_sass() -> str:
     """compiles sass index file"""
     index_file: Path = (Path.cwd().parent / "index.scss")
     return sass.compile(
         filename=str(index_file),
-        custom_functions={custom_flair_images}
+        custom_functions={
+            custom_flair_images,
+            custom_sidebar_exists,
+            custom_sidebar_dimensions,
+            custom_sidebar_name
+        }
     )
 
 def main() -> None:
---
+++ scripts/find_sidebar.py
@@ -0,0 +1,34 @@
+"""finds sidebar name and height"""
+from pathlib import Path
+
+import sass
+
+def _sidebar_path() -> Path:
+    sidebar_dir = (Path.cwd().parent / "assets" / "sidebar")
+    return next((
+        image for image in sidebar_dir.glob("*.png")
+        if not image.name.startswith('_')
+    ), None)
+
+def custom_sidebar_exists() -> bool: 
+    """checks if sidebar image actually exists"""
+    return _sidebar_path() is not None
+
+def custom_sidebar_dimensions() -> sass.SassList:
+    """returns height of sidebar"""
+    from PIL import Image
+
+    sidebar_path: Path = _sidebar_path()
+    return sass.SassList(
+        [sass.SassNumber(dim, unit="px") for dim in Image.open(sidebar_path).size],
+        separator=sass.SASS_SEPARATOR_COMMA
+    )
+
+def custom_sidebar_name() -> str:
+    """returns name of sidebar"""
+    sidebar_path: Path = _sidebar_path()
+    return sidebar_path.stem
+
+if __name__ == "__main__":
+    print(custom_sidebar_name())
+    print(custom_sidebar_dimensions())
