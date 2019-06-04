8e8d13b53f0da580de2420abb262513f5b902ddb
jenbanim <jenbanim@gmail.com>
2019-06-04 01:21:53 -0700

    Finalize deploy_emoji.py
    deploy_emoji.py now only deploys emojis. No messing with existing
    flairs.

--- deploy_emoji.py
+++ deploy_emoji.py
@@ -6,20 +6,33 @@ from PIL import Image
 import praw
 
 
-# Load bot
+prompt = """
+Which sub would you like to deploy to?
+
+1 - wumbotarian
+2 - neoliberal
+
+"""
+
+# Prompt for target sub (wumbotarian or neoliberal)
+target_sub = None
+while target_sub is None:
+    sub_indicator = input(prompt)
+    if sub_indicator == '1':
+        target_sub = 'wumbotarian'
+    elif sub_indicator == '2':
+        target_sub = 'neoliberal'
+
+# Load bot, expected to be simple json dict
 with open('bot_deets.json') as f:
     bot_deets = json.load(f)
-wumbotarian = praw.Reddit(**bot_deets).subreddit('wumbotarian')
+subreddit = praw.Reddit(**bot_deets).subreddit(target_sub)
 
 # Get flairs from assets/flairs directory
 subreddit_css_directory = os.path.dirname(os.path.realpath(__file__))
 flair_directory = os.path.join(subreddit_css_directory, 'assets/flairs')
 flair_filenames = glob.glob(flair_directory + '/**/_*.png')
-flair_names = [
-    os.path.basename(flair)[1:-4]
-    for flair 
-    in flair_filenames
-]
+flair_names = [os.path.basename(flair)[1:-4] for flair in flair_filenames]
 
 #Safety checks -- no duplicate names, correct size
 duplicates = list(set([x for x in flair_names if flair_names.count(x) > 1]))
@@ -31,22 +44,7 @@ for flair_filename in flair_filenames:
         raise Exception('{} is too big (>128x128)'.format(flair_filename))
 
 # Add any missing emojis
-subreddit_emojis = [emoji.name for emoji in wumbotarian.emoji]
+subreddit_emojis = [emoji.name for emoji in subreddit.emoji]
 for idx, flair in enumerate(flair_names):
     if flair not in subreddit_emojis:
         print('adding {}'.format(flair))
-
-# add emojis to userflairs
-for flair in wumbotarian.flair():
-    user = flair['user'].name
-    flair_css = flair['flair_css_class']
-    flair_text = flair['flair_text']
-    if flair_css is not None:
-        # Identify image in user flair css
-        for flair_name in flair_names:
-            if flair_name+' ' in flair_css:
-                flair_image = flair_name
-        emojus = ':'+flair_image+':'
-        if emojus not in flair_text:
-            print('Adding emoji to {}'.format(user))
-            wumbotarian.flair.update([user], emojus+' '+flair_text, flair_css)
